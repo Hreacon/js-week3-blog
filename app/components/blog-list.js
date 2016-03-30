283783fee53df41dff90579c88af3534787badc0
Brad Pizzimenti and Nicholas Jensen-Hay <brad@gennakersystems.com>
2016-03-30 16:28:06 -0700

    end of day wip commit

--- app/components/blog-list.js
+++ app/components/blog-list.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions:{
     deleteMe(blog) {
       this.sendAction("deleteMe", blog);
+    },
+    updateMe(blog) {
+      this.sendAction("updateMe", blog);
     }
   }
 });
